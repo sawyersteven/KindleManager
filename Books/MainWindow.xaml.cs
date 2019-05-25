7629d66efd324dd1381f39046133a54870aa0acf
sawyersteven <sawyerstevenk@gmail.com>
2019-05-24 22:05:57 -0400

    Sortable columns for calculated values InLibrary

--- Books/MainWindow.xaml.cs
+++ Books/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using MahApps.Metro.Controls;
 using System.Windows;
 using System.Linq;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Collections;
 
 namespace Books
 {
@@ -62,5 +67,51 @@ namespace Books
             return Formats.Resources.CompatibleFileTypes.Contains(System.IO.Path.GetExtension(paths[0]));
         }
 
+        /// <summary>
+        /// Custom sort logic for InLibrary columns
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SortLibraryTable(object sender, System.Windows.Controls.DataGridSortingEventArgs e)
+        {
+            DataGridColumn column = e.Column;
+
+            if (column.DisplayIndex == 0 || column.DisplayIndex == 1)
+            {
+                e.Handled = true;
+                ViewModels.MainWindow vm = (ViewModels.MainWindow)DataContext;
+
+                ObservableCollection<Database.BookEntry> library = (column.DisplayIndex == 0) ? vm.LocalLibrary : vm.RemoteLibrary;
+
+                // column.SortDirection can be null, so default to Ascending
+                ListSortDirection direction = (column.SortDirection != ListSortDirection.Ascending) ? ListSortDirection.Ascending : ListSortDirection.Descending;
+                column.SortDirection = direction;
+
+                ListCollectionView books = (ListCollectionView)CollectionViewSource.GetDefaultView(((DataGrid)sender).ItemsSource);
+
+                //apply the sort
+                books.CustomSort = new Sorter(library, direction);
+            }
+
+        }
+
+        private struct Sorter : IComparer
+        {
+            private ListSortDirection direction;
+            private ObservableCollection<Database.BookEntry> library;
+
+            public Sorter(ObservableCollection<Database.BookEntry> l, ListSortDirection d)
+            {
+                direction = d;
+                library = l;
+            }
+
+            public int Compare(object x, object y)
+            {
+                int a = library.Any(z => z.Id == ((Database.BookEntry)x).Id) ? 1 : 0;
+                int b = library.Any(z => z.Id == ((Database.BookEntry)y).Id) ? 1 : 0;
+                return (direction == ListSortDirection.Ascending) ? a - b : b - a;
+            }
+        }
     }
 }
