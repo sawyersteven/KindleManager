b0e097bd31587dd8850b98f42676ea000f7af00f
sawyersteven <sawyerstevenk@gmail.com>
2019-06-17 22:47:40 -0400

    Base32 int converter
    Because mobi versions 8+ use these for absolutely no good reason whatsoever.

---
+++ KindleManager/Utils/Base32Int.cs
@@ -0,0 +1,34 @@
+﻿using ExtensionMethods;
+using System;
+
+namespace Utils
+{
+    class Base32Int
+    {
+
+        public static bool TryParse(string s, out int n)
+        {
+            n = 0;
+            if (s == string.Empty || s == null) return false;
+
+            byte[] b = s.ToUpper().Encode();
+            Array.Reverse(b);
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                int mult = (int)Math.Pow(32, i);
+                int a = b[i] - 48;
+                if (a > 9)
+                {
+                    a -= 7;
+                }
+                if (a > 31)
+                {
+                    return false;
+                }
+                n += a * mult;
+            }
+            return true;
+        }
+    }
+}
